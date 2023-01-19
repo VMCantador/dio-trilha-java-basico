21e81fc3e54e2707377039acbdd6ed1725ea62f7
Vezio <vezio.cantador@etec.sp.gov.br>
2023-01-19 09:41:09 -0300

    exemplo pratico de tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,7 +2,23 @@
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+		
+		/* Variaveis do tipo long sempre deve terminar com L, assim como
+		variaveis do tipo float sempre deve terminar com F.  */
+		
+		// variveis e constantes
+		
+		final double VALOR_DE_PI = 3.14;
+		
+		/* Variaveis que não poderá ter seu valor auterado senpre deve comecar com
+		 a palavre final, e ter seu nome escrito com letras em caixa alta.  */
 
 	}
 
